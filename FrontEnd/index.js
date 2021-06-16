2d4dac2196ea2c4404f754e0913db894f4a9dba8
Myriam Boukarniaa <myriam.boukarniaa@hotmail.com>
2021-06-16 13:47:11 +0200

    creation dossier back et front end

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,3 +1,5 @@
+var products = [];
+
 // toggle navigation//
 var menu = document.getElementById('btn');
 var navItems = document.getElementById('navItems');
@@ -14,55 +16,57 @@ function toggleNav() {
 };
 //toggle navigation/
 
+
+
 //creation product card//
-    function cardOurs(ours){
-        var myTeddy = document.createElement('a');    
-        const bear = document.getElementById('bear');
-        bear.appendChild(myTeddy);
-        myTeddy.setAttribute('class', 'myTeddy');
-        myTeddy.setAttribute('href', 'produit.html?id='+ours._id); 
-        
-        //creation balise img//
-        var myTeddyImg = document.createElement('div');
-        myTeddy.appendChild(myTeddyImg); 
-        myTeddyImg.setAttribute('class', 'teddyImage'); 
-        myTeddyImg.style.backgroundImage = "url("+ours.imageUrl+")";
+function cardOurs(ours){
+    var myTeddy = document.createElement('a');    
+    var bear = document.getElementById('bear');
+    bear.appendChild(myTeddy);
+    myTeddy.setAttribute('class', 'myTeddy');
+    myTeddy.setAttribute('href', 'produit.html?id='+ours._id); 
+    
+    //creation balise img//
+    var myTeddyImg = document.createElement('div');
+    myTeddy.appendChild(myTeddyImg); 
+    myTeddyImg.setAttribute('class', 'teddyImage'); 
+    myTeddyImg.style.backgroundImage = "url("+ours.imageUrl+")";
 
-        //creation div name//
-        var myTeddyName = document.createElement('div');
-        myTeddy.appendChild(myTeddyName); 
-        myTeddyName.setAttribute('class', 'teddyName');
-        myTeddyName.innerHTML  = ours.name;
-        //console.log(ours.name)
+    //creation div name//
+    var myTeddyName = document.createElement('div');
+    myTeddy.appendChild(myTeddyName); 
+    myTeddyName.setAttribute('class', 'teddyName');
+    myTeddyName.innerHTML  = ours.name;
+    //console.log(ours.name)
 
-        //creation div price//
-        var myTeddyPrice = document.createElement('div');
-        myTeddy.appendChild(myTeddyPrice); 
-        myTeddyPrice.setAttribute('class', 'teddyPrice');
-        myTeddyPrice.innerHTML  = ours.price/100 + '€';
+    //creation div price//
+    var myTeddyPrice = document.createElement('div');
+    myTeddy.appendChild(myTeddyPrice); 
+    myTeddyPrice.setAttribute('class', 'teddyPrice');
+    myTeddyPrice.innerHTML  = ours.price/100 + '€';
 
-        //creation div colors//
-        var myTeddyColors = document.createElement('div');
-        myTeddy.appendChild(myTeddyColors); 
-        myTeddyColors.setAttribute('class', 'teddyColors');
-        myTeddyColors.innerHTML  = ours.colors;
+    //creation div colors//
+    var myTeddyColors = document.createElement('div');
+    myTeddy.appendChild(myTeddyColors); 
+    myTeddyColors.setAttribute('class', 'teddyColors');
+    myTeddyColors.innerHTML  = ours.colors;
 
 
-        //creation div info//
-        var myTeddyInfo = document.createElement('div');
-        myTeddy.appendChild(myTeddyInfo); 
-        myTeddyInfo.setAttribute('class', 'teddyInfo');
-        myTeddyInfo.innerHTML  = ours.description;
-    };
+    //creation div info//
+    var myTeddyInfo = document.createElement('div');
+    myTeddy.appendChild(myTeddyInfo); 
+    myTeddyInfo.setAttribute('class', 'teddyInfo');
+    myTeddyInfo.innerHTML  = ours.description;
+};
 
 //creation product card and color selection//
 function cardOursProduit(ours){
 
-        var myTeddy = document.createElement('a');    
-        const bear = document.getElementById('bear');
+        var myTeddy = document.createElement('div');    
+        var bear = document.getElementById('bear');
         bear.appendChild(myTeddy);
         myTeddy.setAttribute('class', 'myTeddy');
-        myTeddy.setAttribute('href', 'produit.html?id='+ours._id); 
+
         
         //creation balise img//
         var myTeddyImg = document.createElement('div');
@@ -86,25 +90,20 @@ function cardOursProduit(ours){
         //creation button color selection
         var btnSelectColor = document.createElement('input');
         myTeddy.appendChild(btnSelectColor);
-        btnSelectColor.setAttribute('id', 'btnColor')
-        btnSelectColor.setAttribute('type', 'button')
-        btnSelectColor.setAttribute('value', 'Sélectionnez votre couleur')
-        //btnSelectColor.setAttribute('onclick', 'toggleColorSelector()')
-        btnSelectColor.innerHTML='sélectionnez votre couleur';
+        btnSelectColor.setAttribute('id', 'btnColor');
+        btnSelectColor.setAttribute('type', 'button');
+        btnSelectColor.setAttribute('value', 'Sélectionnez votre couleur');
 
         //creation list colors and balises a//
         var myTeddyColors = document.createElement('ul');
         myTeddy.appendChild(myTeddyColors); 
-        myTeddyColors.setAttribute('class', 'teddyColors selectColor');
-        
+        myTeddyColors.setAttribute('class', 'selectColor');
+   
             for (i=0; i< ours.colors.length; i++){
             var myTeddyColorsLi = document.createElement('li');
             myTeddyColors.appendChild(myTeddyColorsLi);
             myTeddyColorsLi.setAttribute('class', 'colors');
 
-            function toggleColorSelector(){
-                btnSelectColor.style.backgroundColor='red';
-            };
             // link color 'a'//
             var myTeddyColorsLiA = document.createElement('a')
             myTeddyColorsLi.appendChild(myTeddyColorsLiA)
@@ -118,22 +117,18 @@ function cardOursProduit(ours){
 
 };
 
-//creation card panier//
-function cardOursPanier(ours){
-
-};
-
-
 function getOursList(){
     fetch ('http://localhost:3000/api/teddies')
     .then (res=>res.json())
-    .then (data =>{
+    .then (data =>{ 
+        products = data;
         //console.log(data);
         for (let i=0; i < data.length; i++){
             //console.log("myAPI", data[i])
-            cardOurs(data[i]);      
+            cardOurs(data[i]); 
         }  
-    })
+       
+    })  
 }; 
 
 function getOursById(id){
@@ -144,15 +139,6 @@ function getOursById(id){
         cardOursProduit(data);
     })
 }; 
-
-
-//page produit et selection couleur//
-
-
-
-
-
-
 
 // main//
 var queryString = window.location.search;
@@ -172,8 +158,25 @@ else{
 };
 
 
- 
 
+/*fetch("http://localhost:3000/api/teddies/order", {
+	method: 'POST',
+	headers: { 
+'Accept': 'application/json', 
+'Content-Type': 'application/json' 
+},
+	body: JSON.stringify({
+        contact:{
+            firstName : 'a',
+            lastName: 'b',
+            adress : 'f',
+            city: 'd',
+            email:'c'
+        },
+        products:[products];
+    })
+});*/
+ 
 
 
 
